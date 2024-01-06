5ac1878a45c29097791a450f8099a89dc8bd6cd1
zeid <zeidhassan4@gmail.com>
2024-01-06 18:35:49 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('After DOM Loaded')
+    document.getElementById('text').innerHTML="This is really cool!"
+        console.log(document.querySelector('div'))
+})
+  
